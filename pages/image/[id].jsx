bf7fcbdb8996ec03a8697271dd3ab2a3d04fc4c1
David Heyman <dave@axismaps.com>
2020-07-09 16:28:43 +0100

    Removed incorrect comments

--- pages/image/[id].jsx
+++ pages/image/[id].jsx
@@ -24,13 +24,10 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  // params contains the post `id`.
-  // If the route is like /posts/1, then params.id is 1
   const {
     data: { metadata, thumbnail },
   } = await axios.get(`https://situatedviews.axismaps.io/iiif/${params.id}/manifest`);
 
-  // Pass post data to the page via props
   return { props: { metadata, thumbnail } };
 }
 
