2c6c2c37445b1ee43b9f2a5ee55db8839b0b1258
buccalon <buccalon@users.noreply.github.com>
2020-11-09 00:14:03 -0300

    Fix manifest parsers

--- pages/image/[id].jsx
+++ pages/image/[id].jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import axios from 'axios';
+import Head from 'next/head'
 
-const Image = ({ metadata, thumbnail }) => (
+
+const Image = ({ metadata, ...params }) => (
   <>
+    <Head>
+      <title>{params.id}</title>
+    </Head>
+
+    <img src={`https://situatedviews.axismaps.io/iiif-img/3/${params.id}/pct:1,1,98,98/%5E512,/0/default.jpg`} alt="this is a thumbnail" />
+
     <h1>An Image!</h1>
     {metadata.map(m => (
       <p>{`${m.label.none ? m.label.none[0] : m.label['pt-br'][0]} - ${
@@ -15,20 +23,20 @@ const Image = ({ metadata, thumbnail }) => (
 export async function getStaticPaths() {
   // Get images from IIIF colleciton manifest
   const {
-    data: { items },
-  } = await axios.get('https://situatedviews.axismaps.io/iiif/collection/11');
+    data: { manifests },
+  } = await axios.get('https://situatedviews.axismaps.io/iiif/2/collection/all');
 
-  const paths = items.map(item => `/image/${item.id.match(/\d+(?=\/manifest)/)}`);
+  const paths = manifests.map(manifest => `/image/${manifest["@id"].match(/[^/]+(?=\/manifest)/)}`);
 
   return { paths, fallback: false };
 }
 
 export async function getStaticProps({ params }) {
   const {
-    data: { metadata, thumbnail },
-  } = await axios.get(`https://situatedviews.axismaps.io/iiif/${params.id}/manifest`);
+    data: { metadata },
+  } = await axios.get(`https://situatedviews.axismaps.io/iiif/3/${params.id}/manifest`);
 
-  return { props: { metadata, thumbnail } };
+  return { props: { metadata, ...params } };
 }
 
 export default Image;
--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,37 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Link from 'next/link';
+import axios from 'axios';
 
-export default function Home() {
+export async function getStaticProps() {
+  // Get images from IIIF colleciton manifest
+  const {
+    data: { manifests },
+  } = await axios.get('https://situatedviews.axismaps.io/iiif/2/collection/all');
+
+  const identifiers = manifests.map(manifest => `${manifest['@id'].match(/[^/]+(?=\/manifest)/)}`);
+
+  return { props: { identifiers } };
+}
+
+export default function Home({ identifiers }) {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>IIIF imagineRio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <h1 className="title">IIIF imagineRio</h1>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {identifiers.map(identifier => (
+            <Link href={`/image/${identifier}`}>
+              <a>
+                <h3>{identifier}</h3>
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
 
@@ -54,8 +41,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          Powered by <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
 
@@ -100,6 +86,14 @@ export default function Home() {
         a {
           color: inherit;
           text-decoration: none;
+          margin: 4px;
+          padding: 4px;
+          border-radius: 30px;
+          border: 1px solid grey;
+        }
+
+        h3 {
+          margin: 0;
         }
 
         .title a {
@@ -134,8 +128,8 @@ export default function Home() {
           border-radius: 5px;
           padding: 0.75rem;
           font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
+            Bitstream Vera Sans Mono, Courier New, monospace;
         }
 
         .grid {
@@ -195,9 +189,8 @@ export default function Home() {
         body {
           padding: 0;
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
         }
 
         * {
@@ -205,5 +198,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
