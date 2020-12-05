7d79073ebf02ee30de07782f381493860b1bfcd1
David Heyman <dave@axismaps.com>
2020-12-05 00:59:06 +0000

    Properly configure app page

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
 import React from 'react';
-
 import { ChakraProvider } from '@chakra-ui/react';
 
-function App() {
+function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <App />
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 }
