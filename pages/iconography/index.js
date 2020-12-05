c4fdeb13765c213897c5d7bcf0d50e99296f519d
David Heyman <dave@axismaps.com>
2020-12-05 04:03:44 +0000

    Added iconography page

---
+++ pages/iconography/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Link from 'next/link';
+import { Container, Heading, Box, VStack } from '@chakra-ui/react';
+
+import Head from '../../components/Head';
+import Header from '../../components/Header';
+import Breadcrumbs from '../../components/Breadcrumbs';
+
+import config from '../../utils/config';
+
+const Iconography = () => (
+  <>
+    <Head title="Iconography" />
+    <Header />
+    <Container maxW="5xl">
+      <Breadcrumbs />
+      <Heading>Iconography</Heading>
+      <hr></hr>
+      <VStack>
+        {config.collections.map(collection => (
+          <Box key={collection}>
+            <Heading size="md">
+              <Link href={`/iconography/${collection}`}>{collection}</Link>
+            </Heading>
+          </Box>
+        ))}
+      </VStack>
+    </Container>
+  </>
+);
+
+export default Iconography;
