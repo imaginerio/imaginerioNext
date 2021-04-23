e7b35e6fc939a16c9fc6dc57b635259c3b7d94a3
David Heyman <dave@axismaps.com>
2021-04-23 02:35:48 +0100

    Finished timeline

--- components/ImageFilter/index.js
+++ components/ImageFilter/index.js
@@ -5,19 +5,33 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import { Grid, InputGroup, Input, InputRightAddon, InputRightElement } from '@chakra-ui/react';
 
+import Timeline from '../Timeline';
+
 const ImageFilter = ({ images, handler }) => {
+  const { min, max } = images.reduce(
+    (memo, nextImage) => ({
+      min: Math.min(memo.min, nextImage.firstyear),
+      max: Math.max(memo.max, nextImage.lastyear),
+    }),
+    { min: Infinity, max: -Infinity }
+  );
+
   const [search, setSearch] = useState('');
+  const [dates, setDates] = useState([min, max]);
+
   const fuse = new Fuse(images, {
     keys: ['title', 'creator', 'depicts.value'],
   });
 
   useEffect(() => {
-    if (search) return handler(fuse.search(search).map(f => f.item));
-    return handler(images);
-  }, [search]);
+    let items = images;
+    if (search) items = fuse.search(search).map(f => f.item);
+    items = items.filter(i => i.firstyear <= dates[1] && i.lastyear >= dates[0]);
+    handler(items);
+  }, [search, dates]);
 
   return (
-    <Grid templateColumns="1fr 1fr" mb={2}>
+    <Grid templateColumns="2fr 3fr" gap="20px" mb={2}>
       <InputGroup>
         <Input
           value={search}
@@ -38,6 +52,7 @@ const ImageFilter = ({ images, handler }) => {
           <FontAwesomeIcon icon={faSearch} />
         </InputRightAddon>
       </InputGroup>
+      <Timeline min={min} max={max} handler={setDates} />
     </Grid>
   );
 };
