cbb8fb2315f6a198dfe7bbd06a7022953159a216
David Heyman <dave@axismaps.com>
2021-04-30 11:01:10 +0100

    Smaller grid size

--- components/ImageGrid/index.js
+++ components/ImageGrid/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import dynamic from 'next/dynamic';
-import { Text, Box, Link } from '@chakra-ui/react';
+import Image from 'next/image';
+import { Box, Link, Tooltip } from '@chakra-ui/react';
 
 const FixedSizeGrid = dynamic(() => import('react-window').then(mod => mod.FixedSizeGrid), {
   ssr: false,
 });
 
 const ImageGrid = ({ width, height, activeImages }) => {
-  const numColumns = Math.floor(width / 340);
+  const numColumns = Math.floor(width / 200);
   const gridWidth = (width - 40) / numColumns;
 
   const Grid = ({ rowIndex, columnIndex, style }) => {
@@ -16,27 +17,13 @@ const ImageGrid = ({ width, height, activeImages }) => {
     const { title, thumbnail, ssid, collection } = activeImages[index];
     return (
       <div style={style}>
-        <Link href={`/iconography/${collection}/${ssid}`}>
-          <Box
-            w={`${gridWidth - 40}px`}
-            h="150px"
-            backgroundImage={`url(${thumbnail})`}
-            backgroundSize="contain"
-            backgroundRepeat="no-repeat"
-            backgroundPosition="center"
-            mx="20px"
-          />
-          <Text
-            mx="20px"
-            w={`${gridWidth - 40}px`}
-            overflow="hidden"
-            whiteSpace="nowrap"
-            textOverflow="ellipsis"
-            textAlign="center"
-          >
-            {title}
-          </Text>
-        </Link>
+        <Tooltip label={title} hasArrow>
+          <Link href={`/iconography/${collection}/${ssid}`}>
+            <Box pos="relative" w={`${gridWidth - 40}px`} h="150px" mx="20px">
+              <Image src={thumbnail} layout="fill" objectFit="contain" />
+            </Box>
+          </Link>
+        </Tooltip>
       </div>
     );
   };
@@ -54,7 +41,7 @@ const ImageGrid = ({ width, height, activeImages }) => {
         columnWidth={gridWidth}
         columnCount={numColumns}
         rowCount={Math.ceil(activeImages.length / numColumns)}
-        rowHeight={210}
+        rowHeight={170}
       >
         {Grid}
       </FixedSizeGrid>
