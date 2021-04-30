09411fd7b8757596a4cd7febe0a84e080ecefd8b
David Heyman <dave@axismaps.com>
2021-04-30 10:41:50 +0100

    Font update

--- components/Head/index.js
+++ components/Head/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import NextHead from 'next/head';
-import GoogleFonts from 'next-google-fonts';
 
 const Head = ({ title }) => (
-  <>
-    <GoogleFonts href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" />
-    <NextHead>
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-
-      <title>{title}</title>
-    </NextHead>
-  </>
+  <NextHead>
+    <meta charSet="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Barlow:wght@300;500&display=swap"
+      rel="stylesheet"
+    />
+    <title>{title}</title>
+  </NextHead>
 );
 
 Head.propTypes = {
