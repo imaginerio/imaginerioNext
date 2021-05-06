95c9478c69192e98e9e1f4b15dcd663632fc2fa1
David Heyman <dave@axismaps.com>
2021-05-06 21:26:37 +0100

    Fixed fontawesome and mapbox css

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,19 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 
+import { config } from '@fortawesome/fontawesome-svg-core';
 import { ImageContextProvider } from '../providers/ImageContext';
+
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 import '../components/Timeline/Timeline.css';
 import '../components/Mirador/Mirador.css';
+import '../components/AtlasController/AtlasController.css';
 
 import theme from '../theme/theme';
+
+config.autoAddCss = false;
 
 function App({ Component, pageProps }) {
   return (
