1e9f357746faa06b7d36ba9ececbc08db47b904b
David Heyman <dave@axismaps.com>
2021-05-21 10:24:28 +0100

    Styling fixes

--- components/Legend/index.js
+++ components/Legend/index.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import useSwr from 'swr';
+import { sortBy } from 'lodash';
 import { getLegend } from '@imaginerio/diachronic-atlas';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleRight, faBinoculars } from '@fortawesome/free-solid-svg-icons';
-import { Box, Stack, HStack, Flex, Spacer, Text, Button, Heading } from '@chakra-ui/react';
+import { faAngleRight, faAngleLeft, faBinoculars } from '@fortawesome/free-solid-svg-icons';
+import { Box, Stack, HStack, Flex, Spacer, Text, Heading } from '@chakra-ui/react';
 
 import { useImages } from '../../providers/ImageContext';
 
@@ -17,14 +18,20 @@ const Legend = () => {
   const { data } = useSwr(`${process.env.NEXT_PUBLIC_SEARCH_API}/layers?year=${year}`, fetcher);
 
   const [legend, setLegend] = useState(null);
+  const [legendOpen, setLegendOpen] = useState(false);
 
   useEffect(() => {
     if (data) {
       setLegend(
-        data.map(layer => ({
-          ...layer,
-          types: layer.types.map(type => getLegend({ layer, type, style })),
-        }))
+        sortBy(
+          data
+            .filter(layer => layer.types.filter(t => t).length)
+            .map(layer => ({
+              ...layer,
+              types: layer.types.map(type => getLegend({ layer, type, style })),
+            })),
+          'title'
+        )
       );
     }
   }, [data]);
@@ -33,27 +40,31 @@ const Legend = () => {
     <>
       <Flex
         h="40px"
-        w="200px"
+        w={legendOpen ? '280px' : '200px'}
         pos="absolute"
         mt="20px"
         pl="20px"
         pr="10px"
         zIndex={9}
         backgroundColor="white"
-        boxShadow="sm"
+        boxShadow="md"
         borderRadius="0 4px 4px 0"
         alignItems="center"
+        onClick={() => setLegendOpen(!legendOpen)}
+        cursor="pointer"
       >
         <Text>{`Map Contents: ${year}`}</Text>
         <Spacer />
-        <FontAwesomeIcon icon={faAngleRight} />
+        <FontAwesomeIcon icon={legendOpen ? faAngleLeft : faAngleRight} />
       </Flex>
       <Box>
-        {legend && (
+        {legendOpen && legend && (
           <Box
             pos="absolute"
             h="calc(100vh - 90px)"
+            w="250px"
             px="20px"
+            pb="20px"
             zIndex={9}
             backgroundColor="white"
             overflow="auto"
@@ -66,18 +77,23 @@ const Legend = () => {
                 </Heading>
                 {layer.types.map(type => (
                   <HStack key={type.type} alignItems="center">
-                    <Button
-                      h="30px"
+                    <Flex
                       w="200px"
-                      justifyContent="flex-start"
+                      minH="20px"
+                      px="10px"
+                      py="5px"
                       backgroundColor="#F2F2F2"
                       border="1px solid #DEDEDE"
                       color="black"
+                      fontSize="15px"
+                      lineHeight="18px"
+                      alignItems="center"
+                      borderRadius="4px"
                     >
                       {type.type}
-                      <Spacer />
+                      <Spacer px="10px" />
                       <FontAwesomeIcon icon={faBinoculars} />
-                    </Button>
+                    </Flex>
                     <Box w="40px" h="20px" {...type.swatch} />
                   </HStack>
                 ))}
