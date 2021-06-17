761df0eae399bd53d7320e0df271d9de63b64434
David Heyman <dave@axismaps.com>
2021-06-17 16:42:55 +0100

    Added translation menu

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { Container, Grid, Box, Heading, Text } from '@chakra-ui/react';
@@ -21,59 +20,60 @@ const ContactColumn = () => (
   </Box>
 );
 
-const NavigationColumn = ({ locale }) => (
-  <Box>
-    <Heading size="md">Menu</Heading>
-    <Text>
-      <Link href="/">Home</Link>
-    </Text>
-    {Object.keys(pages[locale]).map(page => (
-      <Text key={page}>
-        <Link href={`${page}`} style={{ textTransform: 'capitalize' }}>
-          {page}
-        </Link>
-      </Text>
-    ))}
-    <Text>
-      <Link href={`/${locale}/iconography`}>Iconography</Link>
-    </Text>
-    <Text>
-      <Link href={`/${locale}/map`}>Map</Link>
-    </Text>
-  </Box>
-);
-
-NavigationColumn.propTypes = {
-  locale: PropTypes.string,
-};
-
-NavigationColumn.defaultProps = {
-  locale: 'en',
-};
-
-const Footer = () => {
+const NavigationColumn = () => {
   const { locale } = useRouter();
   return (
-    <section style={{ backgroundColor: '#F7F9FC', padding: '50px 0' }}>
-      <Container>
-        <Grid templateColumns="1fr repeat(3, 150px)" columnGap="40px">
-          <ContactColumn />
-          <NavigationColumn locale={locale} />
-          <Box>
-            <Heading size="md">Social Media</Heading>
-            <Text>Facebook</Text>
-            <Text>Twitter</Text>
-            <Text>Instagram</Text>
-          </Box>
-          <Box>
-            <Heading size="md">Contact</Heading>
-            <Text>Email</Text>
-            <Text>Phone</Text>
-          </Box>
-        </Grid>
-      </Container>
-    </section>
+    <Box>
+      <Heading size="md">Menu</Heading>
+      <Text>
+        <Link href="/">Home</Link>
+      </Text>
+      {Object.keys(pages[locale]).map(page => (
+        <Text key={page}>
+          <Link href={`${page}`} style={{ textTransform: 'capitalize' }}>
+            {page}
+          </Link>
+        </Text>
+      ))}
+      <Text>
+        <Link href={`/${locale}/iconography`}>Iconography</Link>
+      </Text>
+      <Text>
+        <Link href={`/${locale}/map`}>Map</Link>
+      </Text>
+    </Box>
   );
 };
+
+const Footer = () => (
+  <section style={{ backgroundColor: '#F7F9FC', padding: '50px 0' }}>
+    <Container>
+      <Grid templateColumns="1fr repeat(4, 150px)" columnGap="40px">
+        <ContactColumn />
+        <Box>
+          <Heading size="md">Language</Heading>
+          <Text>
+            <Link href="/en">English</Link>
+          </Text>
+          <Text>
+            <Link href="/pt">Português</Link>
+          </Text>
+        </Box>
+        <NavigationColumn />
+        <Box>
+          <Heading size="md">Social Media</Heading>
+          <Text>Facebook</Text>
+          <Text>Twitter</Text>
+          <Text>Instagram</Text>
+        </Box>
+        <Box>
+          <Heading size="md">Contact</Heading>
+          <Text>Email</Text>
+          <Text>Phone</Text>
+        </Box>
+      </Grid>
+    </Container>
+  </section>
+);
 
 export default Footer;
