5f9c268a4233b360ec5fba0ffa1cecee5983f78a
David Heyman <dave@axismaps.com>
2021-06-17 16:19:24 +0100

    Simplified footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -5,43 +5,51 @@ import { Container, Grid, Box, Heading, Text } from '@chakra-ui/react';
 
 import pages from '../../assets/config/pages';
 
+const ContactColumn = () => (
+  <Box>
+    <Heading size="md">imagineRio</Heading>
+    <Text>
+      1234 Street Address
+      <br />
+      Houston, TX
+      <br />
+      12345
+      <br />
+      United States
+    </Text>
+  </Box>
+);
+
+const NavigationColumn = locale => (
+  <Box>
+    <Heading size="md">Menu</Heading>
+    <Text>
+      <Link href="/">Home</Link>
+    </Text>
+    {Object.keys(pages[locale]).map(page => (
+      <Text key={page}>
+        <Link href={`${page}`} style={{ textTransform: 'capitalize' }}>
+          {page}
+        </Link>
+      </Text>
+    ))}
+    <Text>
+      <Link href={`/${locale}/iconography`}>Iconography</Link>
+    </Text>
+    <Text>
+      <Link href={`/${locale}/map`}>Map</Link>
+    </Text>
+  </Box>
+);
+
 const Footer = () => {
   const { locale } = useRouter();
   return (
     <section style={{ backgroundColor: '#F7F9FC', padding: '50px 0' }}>
       <Container>
         <Grid templateColumns="1fr repeat(3, 150px)" columnGap="40px">
-          <Box>
-            <Heading size="md">imagineRio</Heading>
-            <Text>
-              1234 Street Address
-              <br />
-              Houston, TX
-              <br />
-              12345
-              <br />
-              United States
-            </Text>
-          </Box>
-          <Box>
-            <Heading size="md">Menu</Heading>
-            <Text>
-              <Link href="/">Home</Link>
-            </Text>
-            {Object.keys(pages[locale]).map(page => (
-              <Text key={page}>
-                <Link href={`${page}`} style={{ textTransform: 'capitalize' }}>
-                  {page}
-                </Link>
-              </Text>
-            ))}
-            <Text>
-              <Link href={`/${locale}/iconography`}>Iconography</Link>
-            </Text>
-            <Text>
-              <Link href={`/${locale}/map`}>Map</Link>
-            </Text>
-          </Box>
+          <ContactColumn />
+          <NavigationColumn locale={locale} />
           <Box>
             <Heading size="md">Social Media</Heading>
             <Text>Facebook</Text>
