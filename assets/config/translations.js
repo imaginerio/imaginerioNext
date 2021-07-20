560e9c4639f743669b69a7a9ea79d1214ac80d37
David Heyman <dave@axismaps.com>
2021-07-20 10:49:14 +0100

    Added homepage content from the site

---
+++ assets/config/translations.js
@@ -0,0 +1,76 @@
+const translation = {
+  home: {
+    en: 'Home',
+    pt: 'Início',
+  },
+  iconography: {
+    en: 'Iconography',
+    pt: 'Iconografia',
+  },
+  map: {
+    en: 'Map',
+    pt: 'Mapa',
+  },
+  views: {
+    en: 'Views',
+    pt: 'Vistas',
+  },
+  maps: {
+    en: 'Maps',
+    pt: 'Mapas',
+  },
+  aerials: {
+    en: 'Aerials',
+    pt: 'Vistas Aéreas',
+  },
+  found: {
+    en: 'images found',
+    pt: 'imagens encontradas',
+  },
+  sort: {
+    en: 'sort by',
+    pt: 'ordenar por',
+  },
+  title: {
+    en: 'Title',
+    pt: 'Título',
+  },
+  date: {
+    en: 'Date',
+    pt: 'Data',
+  },
+  creator: {
+    en: 'Creator',
+    pt: 'Criador',
+  },
+  start: {
+    en: 'Start',
+    pt: 'Início',
+  },
+  end: {
+    en: 'End',
+    pt: 'Fim',
+  },
+  contents: {
+    en: 'Map Contents',
+    pt: 'Conteúdo do Mapa',
+  },
+  years: {
+    en: 'Years',
+    pt: 'Anos',
+  },
+  north: {
+    en: 'Reset North',
+    pt: 'Redefinir Norte',
+  },
+  viewMap: {
+    en: 'View the map',
+    pt: 'Ver o mapa',
+  },
+  browseIconografy: {
+    en: 'Browse iconography',
+    pt: 'Explore a iconografia',
+  },
+};
+
+export default translation;
--- pages/index.js
+++ pages/index.js
@@ -1,81 +1,128 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import Image from 'next/image';
+import axios from 'axios';
+import parse from 'html-react-parser';
+import { useRouter } from 'next/router';
 import { Container, Grid, Box, Heading, Text, Button, Link } from '@chakra-ui/react';
 
 import Head from '../components/Head';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-const Home = () => (
-  <>
-    <Head title="imagineRio" />
-    <Header />
-    <section style={{ backgroundColor: '#F7F9FC', padding: '50px 0' }}>
-      <Container>
-        <Grid templateColumns="1fr 300px" columnGap="50px">
-          <Box display="flex" alignItems="center">
+import translations from '../assets/config/translations';
+
+const ParsedContent = ({ content }) => parse(content);
+
+const Home = ({ content: { map, iconography } }) => {
+  const { locale } = useRouter();
+  return (
+    <>
+      <Head title="imagineRio" />
+      <Header />
+      <section style={{ backgroundColor: '#F7F9FC', padding: '50px 0' }}>
+        <Container>
+          <Grid templateColumns="1fr 300px" columnGap="50px">
+            <Box display="flex" alignItems="center">
+              <Box>
+                <Heading size="sm">{translations.map[locale]}</Heading>
+                <Heading size="md">imagineRio</Heading>
+                <Text my={10} as="div">
+                  <ParsedContent content={map} />
+                </Text>
+                <Link href="/map">
+                  <Button bgColor="#3C558E" color="white" px={10}>
+                    {translations.viewMap[locale]}
+                  </Button>
+                </Link>
+              </Box>
+            </Box>
+            <Box p="40px" bgColor="black" position="relative" width="660px" borderRadius="20px">
+              <Image src="/img/screenshot.png" width={660} height={430} />
+            </Box>
+          </Grid>
+        </Container>
+      </section>
+      <section style={{ padding: '50px 0' }}>
+        <Container>
+          <Grid templateColumns="1fr 1fr" columnGap="50px">
             <Box>
-              <Heading size="sm">Map</Heading>
-              <Heading size="md">imagineRio</Heading>
-              <Text my={10}>
-                A searchable digital atlas that illustrates the social and urban evolution of Rio de
-                Janeiro, as it existed and as it was imagined. Lorem ipsum dolor sit amet,
-                consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-                dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
-                dolores et ea rebum. Stet clita kasd gubergren, no sea.
+              <Heading size="sm">{translations.iconography[locale]}</Heading>
+              <Heading size="md">
+                {`${translations.views[locale]}, ${translations.maps[
+                  locale
+                ].toLowerCase()}, plans, ${translations.aerials[locale].toLowerCase()}`}
+              </Heading>
+              <Text my={10} as="div">
+                <ParsedContent content={iconography} />
               </Text>
-              <Link href="/map">
+              <Link href="/iconography">
                 <Button bgColor="#3C558E" color="white" px={10}>
-                  View the map
+                  {translations.browseIconografy[locale]}
                 </Button>
               </Link>
+              <Box my={10}>
+                <Image src="/img/image3.png" width={460} height={286} alt="imagineRio" />
+              </Box>
+              <Box my={10}>
+                <Image src="/img/image.png" width={460} height={460} alt="imagineRio" />
+              </Box>
             </Box>
-          </Box>
-          <Box p="40px" bgColor="black" position="relative" width="660px" borderRadius="20px">
-            <Image src="/img/screenshot.png" width={660} height={430} />
-          </Box>
-        </Grid>
-      </Container>
-    </section>
-    <section style={{ padding: '50px 0' }}>
-      <Container>
-        <Grid templateColumns="1fr 1fr" columnGap="50px">
-          <Box>
-            <Heading size="sm">Iconography</Heading>
-            <Heading size="md">Views, maps, plans, aerials</Heading>
-            <Text my={10}>
-              Views, historical maps, and ground floor plans –from iconographic, cartographic, and
-              architectural archives– are located in both time and space while their visual and
-              spatial data are integrated across a number of databases and servers, including a
-              public repository of images, a geographic information system, an open-source
-              relational database, and a content delivery web system
-            </Text>
-            <Link href="/iconography">
-              <Button bgColor="#3C558E" color="white" px={10}>
-                Browse iconography
-              </Button>
-            </Link>
-            <Box my={10}>
-              <Image src="/img/image3.png" width={460} height={286} alt="imagineRio" />
+            <Box>
+              <Image src="/img/image1.png" width={460} height={460} alt="imagineRio" />
+              <Box my={10}>
+                <Image src="/img/image2.png" width={460} height={320} alt="imagineRio" />
+              </Box>
+              <Box my={10}>
+                <Image src="/img/image4.png" width={460} height={320} alt="imagineRio" />
+              </Box>
             </Box>
-            <Box my={10}>
-              <Image src="/img/image.png" width={460} height={460} alt="imagineRio" />
-            </Box>
-          </Box>
-          <Box>
-            <Image src="/img/image1.png" width={460} height={460} alt="imagineRio" />
-            <Box my={10}>
-              <Image src="/img/image2.png" width={460} height={320} alt="imagineRio" />
-            </Box>
-            <Box my={10}>
-              <Image src="/img/image4.png" width={460} height={320} alt="imagineRio" />
-            </Box>
-          </Box>
-        </Grid>
-      </Container>
-    </section>
-    <Footer />
-  </>
-);
+          </Grid>
+        </Container>
+      </section>
+      <Footer />
+    </>
+  );
+};
+
+Home.propTypes = {
+  content: propTypes.shape({
+    map: propTypes.string.isRequired,
+    iconography: propTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Home;
+
+export async function getStaticProps({ params, locale }) {
+  const pages = {
+    en: {
+      map: 'en-homepage-map/119',
+      iconography: 'en-homepage-iconography/121',
+    },
+    pt: {
+      map: 'pt-homepage-map/120',
+      iconography: 'pt-homepage-iconography/122',
+    },
+  };
+  const headers = {
+    'Api-Key': process.env.NEXT_PUBLIC_PAGE_API,
+    'Api-Username': 'system',
+  };
+
+  const {
+    data: {
+      post_stream: { posts: map },
+    },
+  } = await axios.get(`${process.env.NEXT_PUBLIC_PAGE_URL}${pages[locale].map}.json`, { headers });
+  const {
+    data: {
+      post_stream: { posts: iconography },
+    },
+  } = await axios.get(`${process.env.NEXT_PUBLIC_PAGE_URL}${pages[locale].iconography}.json`, {
+    headers,
+  });
+  return {
+    props: { content: { map: map[0].cooked, iconography: iconography[0].cooked }, ...params },
+  };
+}
