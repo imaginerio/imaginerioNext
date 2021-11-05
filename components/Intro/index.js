464ac4384b66cce0ce6ee355744cde64108ddd22
David Heyman <dave@axismaps.com>
2021-11-05 07:10:49 +0000

    Cookie and button for intro

--- components/Intro/index.js
+++ components/Intro/index.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import cookieCutter from 'cookie-cutter';
 import { useRouter } from 'next/dist/client/router';
 import { Steps } from 'intro.js-react';
+import { IconButton } from '@chakra-ui/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuestion } from '@fortawesome/pro-solid-svg-icons';
 
 import getSteps from './steps';
 
@@ -10,17 +14,43 @@ const Intro = () => {
   const [{ showIntro }, dispatch] = useImages();
   const { locale } = useRouter();
   const [steps, setSteps] = useState([]);
+  const [introCookie, setIntroCookie] = useState(false);
 
-  useEffect(() => setSteps(getSteps(locale)), [showIntro]);
+  useEffect(() => setIntroCookie(Boolean(cookieCutter.get('intro'))), []);
+
+  useEffect(() => {
+    setSteps(getSteps(locale));
+  }, [showIntro]);
 
   return (
     <>
-      {showIntro && (
+      {showIntro && !introCookie ? (
         <Steps
           enabled
           steps={steps}
           initialStep={0}
-          onExit={() => dispatch(['SET_SHOW_INTRO', false])}
+          onExit={stepIndex => {
+            dispatch(['SET_SHOW_INTRO', false]);
+            if (stepIndex === steps.length - 1) {
+              cookieCutter.set('intro', true);
+            }
+          }}
+        />
+      ) : (
+        <IconButton
+          isRound
+          pos="fixed"
+          bottom={15}
+          right={15}
+          zIndex={9}
+          size="sm"
+          boxShadow="lg"
+          color="gray.600"
+          icon={<FontAwesomeIcon icon={faQuestion} />}
+          onClick={() => {
+            dispatch(['SET_SHOW_INTRO', true]);
+            setIntroCookie(false);
+          }}
         />
       )}
     </>
