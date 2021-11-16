f07648831809e54d8e61a5700f0d424b079fd579
David Heyman <dave@axismaps.com>
2021-11-16 15:37:24 +0000

    New IIIF endpoint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: ['images2.imaginerio.org', 'imaginerio-images.s3.us-east-1.amazonaws.com'],
+    domains: ['iiif.imaginerio.org', 'imaginerio-images.s3.us-east-1.amazonaws.com'],
   },
   i18n: {
     locales: ['en', 'pt'],
